b7b2d5ef996b8da72f98ffb490df4fd9029c6c7d
F_edoS <m-f-a1999@yandex.ru>
2026-03-24 20:34:43 +0700

    Fix

--- src/components/popups/code/components/scan/methods/init.ts
+++ src/components/popups/code/components/scan/methods/init.ts
@@ -12,58 +12,71 @@ const init: I['init'] = async function (this: I) {
     const { setStep } = this.props;
 
     window.JWT = getCookie(enums.ACCESS_TOKEN);
-    window.dataMatrixApp = {
-        root: this.parent.current!,
-        config: {
-            duplicateTimeout: 3_000,
-            showConsole: false,
-            apiURL: '/api/ChZScan',
-            catchOnce: true,
-        },
-        getAppRoot: () => this.parent.current!,
-        activate: () => undefined,
-        deactivate: () => undefined,
-        restart: () => undefined,
-        on: {
-            // dataMatrixSuccess: (d) => {
-            //     console.log(d);
-            // },
-            // dataMatrixError: (e) => {
-            //     console.log(e);
-            // },
-            apiSuccess: async () => {
-                await checkAuth({});
-                await setStep('final');
+
+    if (!document.head.querySelector('link[data-reader]')) {
+        const link = document.createElement('link');
+
+        link.setAttribute('data-reader', 'true');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', `${root}/${styleSrc}`);
+
+        document.head.appendChild(link);
+    }
+
+    if (window.dataMatrixApp) {
+        window.dataMatrixApp.root = this.parent.current!;
+        window.dataMatrixApp.getAppRoot = () => this.parent.current!;
+    }
+
+    if (!document.head.querySelector('script[data-reader]')) {
+        window.dataMatrixApp = {
+            root: this.parent.current!,
+            config: {
+                duplicateTimeout: 3_000,
+                showConsole: false,
+                apiURL: '/api/ChZScan',
+                catchOnce: true,
             },
-            apiError: async (e) => {
-                await setStep('error', e?.response?.data?.errorText || 'Ошибка сервера');
+            getAppRoot: () => this.parent.current!,
+            activate: () => undefined,
+            deactivate: () => undefined,
+            restart: () => undefined,
+            on: {
+                // dataMatrixSuccess: (d) => {
+                //     console.log(d);
+                // },
+                // dataMatrixError: (e) => {
+                //     console.log(e);
+                // },
+                apiSuccess: async () => {
+                    await checkAuth({});
+                    await setStep('final');
+                },
+                apiError: async (e) => {
+                    await setStep('error', e?.response?.data?.errorText || 'Ошибка сервера');
+                },
             },
-        },
-    };
+        };
 
-    const link = document.createElement('link');
+        const script = document.createElement('script');
 
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', `${root}/${styleSrc}`);
+        script.onload = async () => {
+            window.dataMatrixApp!.activate();
+            await this.asyncSetState({ isInit: true });
+        };
 
-    document.head.appendChild(link);
+        script.setAttribute('data-reader', 'true');
+        script.src = `${root}/${scriptSrc}`;
 
-    const script = document.createElement('script');
-
-    script.onload = async () => {
+        document.head.appendChild(script);
+    } else {
         window.dataMatrixApp!.activate();
         await this.asyncSetState({ isInit: true });
-    };
-
-    script.src = `${root}/${scriptSrc}`;
-
-    document.head.appendChild(script);
+    }
 
     this.unmountHandlers.all = () => {
-        link.remove();
-        script.remove();
-
         if (window.dataMatrixApp?.deactivate) {
+            console.log('deac');
             window.dataMatrixApp.deactivate();
         }
     };
