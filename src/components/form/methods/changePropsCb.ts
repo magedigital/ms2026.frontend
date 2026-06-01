a638a7cda541db1d3167849152b14165a871ec64
F_edoS <m-f-a1999@yandex.ru>
2026-06-01 18:40:18 +0700

    Fix anket

--- src/components/form/methods/changePropsCb.ts
+++ src/components/form/methods/changePropsCb.ts
@@ -3,7 +3,7 @@ import I from '../types.ts';
 const changePropsCb: I['changePropsCb'] = async function (this: I, key) {
     if (key === 'fieldsKey') {
         const { data } = this.props;
-        await this.initTarget({ data: { ...data }, targetName: 'form' });
+        await this.initTarget({ data: { ...this.state.form, ...data }, targetName: 'form' });
     }
 };
 
