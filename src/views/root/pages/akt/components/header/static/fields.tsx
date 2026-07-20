7810af5a025edabd97d12f9a6c58506d584a1a56
F_edoS <m-f-a1999@yandex.ru>
2026-07-20 21:34:31 +0700

    Fix checkbox text bag

--- src/views/root/pages/akt/components/header/static/fields.tsx
+++ src/views/root/pages/akt/components/header/static/fields.tsx
@@ -14,7 +14,7 @@ export const aktFields = {
                     <a href="/upload/docs/politics-anket.pdf" target="_blank">
                         политикой конфиденциальности
                     </a>{' '}
-                    и\xa0условиями обработки персональных данных*
+                    и&nbsp;условиями обработки персональных данных*
                 </>
             ),
         },
